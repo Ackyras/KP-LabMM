269019717cec0d2d856ed854c34124ccdd9d88c7
wdftr <ikhbal.madrid17@gmail.com>
2021-03-02 15:59:24 +0700

    update view create barang

--- resources/views/dashboard/inventaris/create.blade.php
+++ resources/views/dashboard/inventaris/create.blade.php
@@ -18,18 +18,17 @@ Tambah Data Inventaris
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Lokasi Barang</label>
         <select name="lokasi" class="form-select">
-            <option selected disabled value="0">Pilih Lokasi Barang</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+            <option selected disabled value="0" {{ old('lokasi') == 0 ? 'selected' : '' }}>Pilih Lokasi Barang</option>
+            <option value="tpb" {{ old('lokasi') == 'tpb' ? 'selected' : '' }}>TPB</option>
+            <option value="prodi" {{ old('lokasi') == 'prodi' ? 'selected' : '' }}>Prodi</option>
         </select>
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Kategori Barang</label>
         <select name="kategori" class="form-select">
-            <option selected disabled value="0">Pilih Kategori Barang</option>
-            <option value="1">Elektronik</option>
-            <option value="2">Non Elektronik</option>
+            <option selected disabled value="0" {{ old('kategori') == 0 ? 'selected' : '' }}>Pilih Kategori Barang</option>
+            <option value="elektronik" {{ old('kategori') == 'elektronik' ? 'selected' : '' }}>Elektronik</option>
+            <option value="nonelektronik" {{ old('kategori') == 'nonelektronik' ? 'selected' : '' }}>Non Elektronik</option>
         </select>
     </div>
     <div class="mb-1 mx-auto form-admin">
@@ -37,11 +36,15 @@ Tambah Data Inventaris
         <input type="text" name="stok" value="{{ old('stok') }}" class="form-control" required>
     </div>
     <div class="mb-1 mx-auto form-admin">
+        <label class="form-label">Peminjaman Barang</label>
+        <input type="text" name="peminjaman" value="{{ old('peminjaman') }}" class="form-control" required>
+    </div>
+    <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Status Barang</label>
         <select name="status" class="form-select">
-            <option selected disabled value="0">Pilih Status Barang</option>
-            <option value="1">Tidak bisa dipinjam</option>
-            <option value="2">Bisa dipinjam</option>
+            <option selected disabled value="0" {{ old('status') == 0 ? 'selected' : '' }}>Pilih Status Barang</option>
+            <option value="Baik" {{ old('status') == 'Baik' ? 'selected' : '' }}>Baik</option>
+            <option value="Tidak Baik" {{ old('status') == 'Tidak Baik' ? 'selected' : '' }}>Tidak Baik</option>
         </select>
     </div>
     <div class="mb-1 mx-auto form-admin">
@@ -50,7 +53,7 @@ Tambah Data Inventaris
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Foto Barang</label>
-        <input class="form-control" type="file" id="formFile">
+        <input class="form-control" name="foto" type="file" id="formFile">
     </div>
     <div class="my-3 mx-auto form-admin position-relative pb-5">
         <button type="submit" class="btn btn-primary position-absolute top-0 end-0">Submit</button>
