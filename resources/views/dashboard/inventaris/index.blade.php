3752938c21503a69c7a7b79eb0fab2c2acd76cdf
wdftr <ikhbal.madrid17@gmail.com>
2021-03-02 15:59:46 +0700

    index view barang

--- resources/views/dashboard/inventaris/index.blade.php
+++ resources/views/dashboard/inventaris/index.blade.php
@@ -10,5 +10,14 @@ Inventaris
         <a class="text-white" href="{{ route('inventaris.create') }}">Tambah Inventaris</a>
         <i class="fas fa-plus"></i>
     </div>
+    @foreach($data as $data)
+    <div class="card mx-3 my-3" style="width: 12rem; height: 280px;">
+        <img src="{{ $data->foto }}" class="card-img-top mt-3" style="object-fit: contain; width: 10rem; height: 10rem;" alt="{{ $data->kd_barang }}">
+        <div class="card-body">
+            <h5 class="card-title mx-auto">{{ $data->nama_barang }}</h5>
+            <a href="{{ route('inventaris.show', $data->id) }}" class="btn btn-primary">Lihat Barang</a>
+        </div>
+    </div>
+    @endforeach
 </div>
 @endsection
