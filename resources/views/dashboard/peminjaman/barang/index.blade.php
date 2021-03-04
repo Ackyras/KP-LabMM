ecce376939e43566f636f0e9dae97943cca67726
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-03-04 15:52:11 +0700

    add view index and riwayat peminjaman barang

---
+++ resources/views/dashboard/peminjaman/barang/index.blade.php
@@ -0,0 +1,9 @@
+@extends('master.dashboard')
+
+@section('title-page')
+Peminjaman Barang
+@endsection
+
+@section('content')
+index list peminjaman barang
+@endsection('content')
---
+++ resources/views/dashboard/peminjaman/barang/riwayat.blade.php
@@ -0,0 +1,9 @@
+@extends('master.dashboard')
+
+@section('title-page')
+Riwayat Peminjaman Barang
+@endsection
+
+@section('content')
+history barang
+@endsection('content')
