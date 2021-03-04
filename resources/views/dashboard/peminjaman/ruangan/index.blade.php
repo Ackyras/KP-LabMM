8b9e5763d510e4cdcb9c96e34fc52e954ae9e2ce
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-03-04 15:52:25 +0700

    add view index and riwayat peminjaman ruangan

---
+++ resources/views/dashboard/peminjaman/ruangan/index.blade.php
@@ -0,0 +1,9 @@
+@extends('master.dashboard')
+
+@section('title-page')
+Peminjaman Barang
+@endsection
+
+@section('content')
+index list peminjaman ruangan
+@endsection('content')
---
+++ resources/views/dashboard/peminjaman/ruangan/riwayat.blade.php
@@ -0,0 +1,9 @@
+@extends('master.dashboard')
+
+@section('title-page')
+Riwayat Peminjaman Barang
+@endsection
+
+@section('content')
+history ruangan
+@endsection('content')
