91276bfeb6dcc95f68114721a24356dea1e7a38b
wdftr <ikhbal.madrid17@gmail.com>
2021-03-09 16:54:33 +0700

    add peminjaman barang controller

---
+++ app/Http/Controllers/Dashboard/PeminjamanBarangController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PeminjamanBarangController extends Controller
+{
+    public function index()
+    {
+        return view(
+            'dashboard.peminjaman.barang.index',
+            [
+                'data' => DB::table('form')
+                    ->where('validasi', 'like', '1')
+                    ->orWhere('validasi', 'like', '2')
+                    ->paginate(20)
+            ]
+        );
+    }
+
+    public function show($id)
+    {
+        return view(
+            'dashboard.peminjaman.barang.show',
+            [
+                'data' => DB::table('form')
+                    ->where('id', $id)
+            ]
+        );
+    }
+
+    public function status(Request $request, $id)
+    {
+        switch ($request->input('action')) {
+            case '2':
+                try {
+                    DB::table('form')->where('id', $id)->update([
+                        'validasi'      => '2'
+                    ]);
+                } catch (Exception $th) {
+                    return redirect()->route('peminjaman.barang')->with('msg', 'Gagal merubah status');
+                }
+                break;
+            case '0':
+                try {
+                    DB::table('form')->where('id', $id)->update([
+                        'validasi'      => '0'
+                    ]);
+                } catch (Exception $th) {
+                    return redirect()->route('peminjaman.barang')->with('msg', 'Gagal merubah status');
+                }
+                break;
+        }
+        return redirect()->route('peminjaman.barang')->with('msg', 'Berhasil merubah status');
+    }
+
+    public function riwayat()
+    {
+        return view(
+            'dashboard.peminjaman.barang.riwayat',
+            [
+                'data' => DB::table('form')
+                    ->where('validasi', 0)
+                    ->paginate(20)
+            ]
+        );
+    }
+}
