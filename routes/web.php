bcc6925ce5dac24f12e5a13d4f0c58a784beed6c
ackyras <ackyrasibarani@gmail.com>
2021-03-12 10:10:41 +0700

    adding auth routing|with grouping

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\authcontroller;
 use App\Http\Controllers\pendaftarcontroller;
 use App\Http\Controllers\BarangController;
 use Illuminate\Support\Facades\Route;
@@ -17,9 +18,23 @@ use App\Http\Controllers\Dashboard\InventarisController;
 */
 // Home
 Route::get('/', function () {
-    return view('barang.index');
+    return view('welcome');
 })->name('home');
 
+// auth
+Route::get('login', [authcontroller::class, 'indexlogin'])->name('login');
+Route::get('login', [authcontroller::class, 'proseslogin'])->name('login.proses');
+Route::get('logout', [authcontroller::class, 'logout'])->name('logout');
+
+Route::group(['middleware'=>['auth']], function(){
+    Route::group(['middleware'=>['Cek_login:admin']], function(){
+
+
+    });
+    Route::group(['middleware'=>['Cek_login:user']], function(){
+
+    });
+});
 
 // Route Client Peminjaman Barang
 Route::get('daftarbarang',              [BarangController::class, 'list'])->name('list');
