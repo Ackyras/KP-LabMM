a3d76936e786dba9048d39aaf948e8959b9e86eb
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-03-20 20:00:10 +0700

    update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(akunSeeder::class);
         $this->call(asprakSeeder::class);
         $this->call(BarangSeeder::class);
+        $this->call(FormPeminjam::class);
+        $this->call(PeminjamanBarang::class);
     }
 }
