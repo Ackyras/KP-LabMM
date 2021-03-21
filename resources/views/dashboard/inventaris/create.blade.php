1bd1c6e2abfc555511cd96ffde70561c45016ef9
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-03-21 16:09:42 +0700

    add error message to create and edit form barang

--- resources/views/dashboard/inventaris/create.blade.php
+++ resources/views/dashboard/inventaris/create.blade.php
@@ -9,51 +9,78 @@ Tambah Data Inventaris
     @csrf
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Kode Barang</label>
-        <input type="text" name="kd_barang" value="{{ old('kd_barang') }}" class="form-control">
+        <input type="text" name="kd_barang" value="{{ old('kd_barang') }}" class="form-control @error('kd_barang') is-invalid @enderror" autofocus>
+        @error('kd_barang')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Nama Barang</label>
-        <input type="text" name="nama_barang" value="{{ old('nama_barang') }}" class="form-control">
+        <input type="text" name="nama_barang" value="{{ old('nama_barang') }}" class="form-control @error('nama_barang') is-invalid @enderror">
+        @error('nama_barang')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Lokasi Barang</label>
-        <select name="lokasi" class="form-select">
+        <select name="lokasi" class="form-select @error('lokasi') is-invalid @enderror">
             <option selected disabled value="" {{ old('lokasi') == '' ? 'selected' : '' }}>Pilih Lokasi Barang</option>
             <option value="TPB" {{ old('lokasi') == 'TPB' ? 'selected' : '' }}>TPB</option>
             <option value="PRODI" {{ old('lokasi') == 'PRODI' ? 'selected' : '' }}>Prodi</option>
         </select>
+        @error('lokasi')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Kategori Barang</label>
-        <select name="kategori" class="form-select">
+        <select name="kategori" class="form-select @error('kategori') is-invalid @enderror">
             <option selected disabled value="" {{ old('kategori') == '' ? 'selected' : '' }}>Pilih Kategori Barang</option>
             <option value="Elektronik" {{ old('kategori') == 'Elektronik' ? 'selected' : '' }}>Elektronik</option>
             <option value="Non Elektronik" {{ old('kategori') == 'Non Elektronik' ? 'selected' : '' }}>Non Elektronik</option>
         </select>
+        @error('kategori')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Stok Barang</label>
-        <input type="text" name="stok" value="{{ old('stok') }}" class="form-control">
+        <input type="number" name="stok" value="{{ old('stok') }}" class="form-control @error('stok') is-invalid @enderror">
+        @error('stok')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Peminjaman Barang</label>
-        <input type="text" name="peminjaman" value="{{ old('peminjaman') }}" class="form-control">
+        <input type="number" name="peminjaman" value="{{ old('peminjaman') }}" class="form-control @error('peminjaman') is-invalid @enderror">
+        @error('peminjaman')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Status Barang</label>
-        <select name="status" class="form-select">
+        <select name="status" class="form-select @error('status') is-invalid @enderror">
             <option selected disabled value="" {{ old('status') == '' ? 'selected' : '' }}>Pilih Status Barang</option>
             <option value="Baik" {{ old('status') == 'Baik' ? 'selected' : '' }}>Baik</option>
             <option value="Tidak Baik" {{ old('status') == 'Tidak Baik' ? 'selected' : '' }}>Tidak Baik</option>
         </select>
+        @error('status')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Masuk Barang</label>
-        <input class="form-control" type="date" name="masuk_barang" value="<?php echo date("Y-m-d"); ?>" id="example-datetime-local-input">
+        <input class="form-control @error('masuk_barang') is-invalid @enderror" type="date" name="masuk_barang" value="<?php echo date("Y-m-d"); ?>" id="example-datetime-local-input">
+        @error('masuk_barang')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Foto Barang</label>
-        <input class="form-control" name="foto" type="file" id="formFile">
+        <input class="form-control @error('foto') is-invalid @enderror" name="foto" type="file" id="formFile">
+        @error('foto')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="my-3 mx-auto form-admin position-relative pb-5">
         <button type="submit" class="btn btn-primary position-absolute top-0 end-0">Submit</button>
--- resources/views/dashboard/inventaris/edit.blade.php
+++ resources/views/dashboard/inventaris/edit.blade.php
@@ -10,52 +10,79 @@ Tambah Data Inventaris
     @method('PUT')
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Kode Barang</label>
-        <input type="text" name="kd_barang" value="{{ $data->kd_barang }}" class="form-control" required>
+        <input type="text" name="kd_barang" value="{{ $data->kd_barang }}" class="form-control @error('kd_barang') is-invalid @enderror" required>
+        @error('kd_barang')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Nama Barang</label>
-        <input type="text" name="nama_barang" value="{{ $data->nama_barang }}" class="form-control" required>
+        <input type="text" name="nama_barang" value="{{ $data->nama_barang }}" class="form-control @error('nama_barang') is-invalid @enderror" required>
+        @error('nama_barang')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Lokasi Barang</label>
-        <select name="lokasi" class="form-select">
+        <select name="lokasi" class="form-select @error('lokasi') is-invalid @enderror">
             <option selected disabled value="" {{ $data->lokasi == '' ? 'selected' : '' }}>Pilih Lokasi Barang</option>
             <option value="TPB" {{ $data->lokasi == 'TPB' ? 'selected' : '' }}>TPB</option>
             <option value="PRODI" {{ $data->lokasi == 'PRODI' ? 'selected' : '' }}>Prodi</option>
         </select>
+        @error('lokasi')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Kategori Barang</label>
-        <select name="kategori" class="form-select">
+        <select name="kategori" class="form-select @error('kategori') is-invalid @enderror">
             <option selected disabled value="" {{ $data->kategori == '' ? 'selected' : '' }}>Pilih Kategori Barang</option>
             <option value="Elektronik" {{ $data->kategori == 'Elektronik' ? 'selected' : '' }}>Elektronik</option>
             <option value="Non Elektronik" {{ $data->kategori == 'Non Elektronik' ? 'selected' : '' }}>Non Elektronik</option>
         </select>
+        @error('kategori')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Stok Barang</label>
-        <input type="text" name="stok" value="{{ $data->stok }}" class="form-control" required>
+        <input type="number" name="stok" value="{{ $data->stok }}" class="form-control @error('stok') is-invalid @enderror" required>
+        @error('stok')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Peminjaman Barang</label>
-        <input type="text" name="peminjaman" value="{{ $data->peminjaman }}" class="form-control" required>
+        <input type="number" name="peminjaman" value="{{ $data->peminjaman }}" class="form-control @error('peminjaman') is-invalid @enderror" required>
+        @error('peminjaman')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Status Barang</label>
-        <select name="status" class="form-select">
+        <select name="status" class="form-select @error('status') is-invalid @enderror">
             <option selected disabled value="" {{ $data->status == '' ? 'selected' : '' }}>Pilih Status Barang</option>
             <option value="Baik" {{ $data->status == 'Baik' ? 'selected' : '' }}>Baik</option>
             <option value="Tidak Baik" {{ $data->status == 'Tidak Baik' ? 'selected' : '' }}>Tidak Baik</option>
         </select>
+        @error('status')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Masuk Barang</label>
-        <input class="form-control" type="date" name="masuk_barang" value="{{ $data->masuk_barang }}" id="example-datetime-local-input">
+        <input class="form-control @error('masuk_barang') is-invalid @enderror" type="date" name="masuk_barang" value="{{ $data->masuk_barang }}" id="example-datetime-local-input">
+        @error('masuk_barang')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="mb-1 mx-auto form-admin">
         <label class="form-label">Foto Barang</label>
-        <input class="form-control" name="foto" type="file" id="formFile">
+        <input class="form-control @error('foto') is-invalid @enderror" name="foto" type="file" id="formFile">
         <input type="hidden" name="oldfile" value="{{ $data->foto }}">
+        @error('foto')
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+        @enderror
     </div>
     <div class="my-3 mx-auto form-admin position-relative pb-5">
         <button type="submit" class="btn btn-primary position-absolute top-0 end-0">Submit</button>
