4e0c5181e1441297bc07cfa41dcafac4882a0839
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-03-21 13:25:12 +0700

    add table calon_aspraks

---
+++ database/migrations/2021_03_21_060742_create_calon_aspraks_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalonAspraksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calon_aspraks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('nim');
+            $table->string('email');
+            $table->date('tanggal_lahir');
+            $table->string('program_studi');
+            $table->integer('angkatan');
+            $table->string('cv');
+            $table->string('khs');
+            $table->string('ktm');
+            $table->string('pil1');
+            $table->string('pil2');
+            $table->string('pil3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calon_aspraks');
+    }
+}
