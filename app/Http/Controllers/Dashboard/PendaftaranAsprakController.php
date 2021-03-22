0c37e8b1ddbc88db3a7e4453a256398256875ce5
wdftr <ikhbal.madrid17@gmail.com>
2021-03-22 15:40:39 +0700

    add PendaftaranAsprakController

---
+++ app/Http/Controllers/Dashboard/PendaftaranAsprakController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\MataKuliah;
+use App\Models\PembukaanAsprak;
+use Illuminate\Http\Request;
+
+class PendaftaranAsprakController extends Controller
+{
+    public function index()
+    {
+        $daftars = PembukaanAsprak::latest()->get();
+        return view('dashboard.pendaftaran.index', compact('daftars'));
+    }
+
+    public function show($id)
+    {
+        $pembukaan = PembukaanAsprak::findOrFail($id);
+        $daftars = MataKuliah::where('pembukaan_asprak_id', $id)->get();
+        return view('dashboard.pendaftaran.show', compact('daftars', 'pembukaan'));
+    }
+
+    public function create()
+    {
+        return view('dashboard.pendaftaran.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'judul'             => 'required',
+            'awal_pembukaan'    => 'required',
+            'akhir_pembukaan'   => 'required'
+        ]);
+        PembukaanAsprak::create($request->all());
+
+        return redirect()->route('rekrut.index')->with('pesan', 'Perekrutan berhasil dibuka');
+    }
+
+    public function edit($id)
+    {
+        $daftar = PembukaanAsprak::findOrFail($id);
+        return view('dashboard.pendaftaran.edit', compact('daftar'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'judul'             => 'required',
+            'awal_pembukaan'    => 'required',
+            'akhir_pembukaan'   => 'required'
+        ]);
+        PembukaanAsprak::where('id', $id)
+            ->update($request->only(['judul', 'awal_pembukaan', 'akhir_pembukaan']));
+
+        return redirect()->route('rekrut.index')->with('pesan', 'Perekrutan berhasil diubah');
+    }
+
+    public function destroy($id)
+    {
+        PembukaanAsprak::where('id', $id)->delete();
+
+        return redirect()->route('rekrut.index')->with('pesan', 'Perekrutan berhasil dihapus');
+    }
+}
