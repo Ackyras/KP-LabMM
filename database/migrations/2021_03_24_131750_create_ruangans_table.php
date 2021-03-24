843198cf7ccc2ab195c51f2b40522627981cad33
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-03-24 20:53:56 +0700

    add ruangans table

---
+++ database/migrations/2021_03_24_131750_create_ruangans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRuangansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ruangans', function (Blueprint $table) {
+            $table->id();
+            $table->time("waktu");
+            $table->string("hari");
+            $table->integer("minggu");
+            $table->integer("status")->default(0);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ruangans');
+    }
+}
