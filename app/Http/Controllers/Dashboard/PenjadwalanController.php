a7f611ddcfc8b3664e7a2204abbeca7eb403743a
wdftr <ikhbal.madrid17@gmail.com>
2021-03-25 12:06:48 +0700

    add PenjadwalanController

---
+++ app/Http/Controllers/Dashboard/PenjadwalanController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\PeminjamanRuangan;
+use App\Models\Ruangan;
+use Illuminate\Http\Request;
+
+class PenjadwalanController extends Controller
+{
+    public function index()
+    {
+        $ruangans = Ruangan::with('peminjamanruangans')->get();
+        return view('dashboard.penjadwalan.index', compact('ruangans'));
+    }
+
+    public function destroy($id)
+    {
+        Ruangan::where('id', $id)->update([
+            'status'    => 0
+        ]);
+        PeminjamanRuangan::where('ruangan_id', $id)->delete();
+
+        $ruangans = Ruangan::with('peminjamanruangans')->get();
+        return view('dashboard.penjadwalan.index', compact('ruangans'));
+    }
+
+    public function massReset()
+    {
+        Ruangan::where('status', 1)->update(['status' => 0]);
+        return view('dashboard.penjadwalan.index', compact('ruangans'));
+    }
+}
