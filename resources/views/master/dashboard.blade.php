0d4d8b1527a35cb69b35e25a21bef530632081bb
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-04-05 00:27:27 +0700

    update link dashboard

--- resources/views/master/dashboard.blade.php
+++ resources/views/master/dashboard.blade.php
@@ -65,7 +65,8 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <!-- <h6 class="collapse-header">Inventaris</h6> -->
-                        <a class="collapse-item" href="{{ route('inventaris.index') }}">Barang</a>
+                        <a class="collapse-item" href="{{ route('inventaris.index') }}">List Barang</a>
+                        <a class="collapse-item" href="{{ route('inventaris.create') }}">Tambah Barang</a>
                     </div>
                 </div>
             </li>
