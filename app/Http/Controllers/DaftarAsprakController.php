d3ffbefec3dec4985e5d218b382b47c54ab3d527
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-04-07 00:41:34 +0700

    update daftarasprak controller

--- app/Http/Controllers/DaftarAsprakController.php
+++ app/Http/Controllers/DaftarAsprakController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\CalonAsprak;
 use App\Models\MataKuliah;
 use App\Models\PembukaanAsprak;
+use App\Models\PenilaianAsprak;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class DaftarAsprakController extends Controller
@@ -17,22 +19,29 @@ class DaftarAsprakController extends Controller
         return view('asprak.index', compact('matakuliahs'));
     }
 
+    public function form()
+    {
+        $pembukaan = PembukaanAsprak::latest()->pluck('id')->first();
+        $matakuliahs = MataKuliah::with('daftarmatakuliah')
+            ->where('pembukaan_asprak_id', $pembukaan)
+            ->get();
+        return view('asprak.form', compact('matakuliahs'));
+    }
+
     public function store(Request $request)
     {
+        // dd($request->all());
         $request->validate(
             [
                 'nama'          => ['required'],
                 'nim'           => ['required', 'regex:/[0-9]+/', 'min:8', 'max:9'],
                 'email'         => ['required', 'email'],
                 'tanggal_lahir' => ['required', 'date'],
-                'program_studi' => ['required'],
+                'prodi'         => ['required'],
                 'angkatan'      => ['required', 'regex:/[0-9]+/'],
-                'cv'            => ['mimes:pdf,jpeg,png,jpg', 'max:512'],
-                'khs'           => ['mimes:pdf,jpeg,png,jpg', 'max:512'],
-                'ktm'           => ['mimes:pdf,jpeg,png,jpg', 'max:512'],
-                'pil1'          => ['required'],
-                'pil2'          => ['nullable'],
-                'pil3'          => ['nullable']
+                'cv'            => ['mimes:pdf,jpeg,png,jpg', 'max:512', 'required'],
+                'khs'           => ['mimes:pdf,jpeg,png,jpg', 'max:512', 'required'],
+                'ktm'           => ['mimes:pdf,jpeg,png,jpg', 'max:512', 'required']
             ]
         );
 
@@ -42,35 +51,42 @@ class DaftarAsprakController extends Controller
             $ktm = $request->file('ktm');
             $path = 'public/calon/' . $request->input('nim');
 
-            $storecv = $cv->storeAs($path, $cv->getClientOriginalName() . '.' . $cv->getClientOriginalExtension());
-            $storekhs = $khs->storeAs($path, $khs->getClientOriginalName() . '.' . $khs->getClientOriginalExtension());
-            $storektm = $ktm->storeAs($path, $ktm->getClientOriginalName() . '.' . $ktm->getClientOriginalExtension());
+            $storecv = $cv->storeAs($path, $cv->getClientOriginalName());
+            $storekhs = $khs->storeAs($path, $khs->getClientOriginalName());
+            $storektm = $ktm->storeAs($path, $ktm->getClientOriginalName());
 
-            $link_cv = $request->root() . 'storage/calon/' . $request->input('nim') . '/' . $cv->getClientOriginalName() . '.' . $cv->getClientOriginalExtension();
-            $link_khs = $request->root() . 'storage/calon/' . $request->input('nim') . '/' . $khs->getClientOriginalName() . '.' . $khs->getClientOriginalExtension();
-            $link_ktm = $request->root() . 'storage/calon/' . $request->input('nim') . '/' . $ktm->getClientOriginalName() . '.' . $ktm->getClientOriginalExtension();
+            $link_cv = $request->root() . '/storage/calon/' . $request->input('nim') . '/' . $cv->getClientOriginalName();
+            $link_khs = $request->root() . '/storage/calon/' . $request->input('nim') . '/' . $khs->getClientOriginalName();
+            $link_ktm = $request->root() . '/storage/calon/' . $request->input('nim') . '/' . $ktm->getClientOriginalName();
 
             $cv = Storage::url($storecv);
             $khs = Storage::url($storekhs);
             $ktm = Storage::url($storektm);
         }
-
-        CalonAsprak::create(
-            [
-                'nama'              => $request->input('nama'),
-                'nim'               => $request->input('nim'),
-                'email'             => $request->input('email'),
-                'tanggal_lahir'     => $request->input('tanggal_lahir'),
-                'program_studi'     => $request->input('program_studi'),
-                'angkatan'          => $request->input('angkatan'),
-                'cv'                => $link_cv,
-                'khs'               => $link_khs,
-                'ktm'               => $link_ktm,
-                'pil1'              => $request->input('pil1'),
-                'pil2'              => $request->input('pil2'),
-                'pil3'              => $request->input('pil3'),
-            ]
-        );
+        DB::transaction(function () use ($request, $link_cv, $link_khs, $link_ktm) {
+            $id = CalonAsprak::create(
+                [
+                    'nama'              => $request->input('nama'),
+                    'nim'               => $request->input('nim'),
+                    'email'             => $request->input('email'),
+                    'tanggal_lahir'     => $request->input('tanggal_lahir'),
+                    'program_studi'     => $request->input('prodi'),
+                    'angkatan'          => $request->input('angkatan'),
+                    'cv'                => $link_cv,
+                    'khs'               => $link_khs,
+                    'ktm'               => $link_ktm
+                ]
+            );
+            $pilihan = $request->input('pilihan');
+            foreach ($pilihan as $key => $value) {
+                PenilaianAsprak::create(
+                    [
+                        'calon_asprak_id'   => $id->id,
+                        'mata_kuliah_id'    => $value
+                    ]
+                );
+            }
+        });
 
         return redirect()->route('home');
     }
