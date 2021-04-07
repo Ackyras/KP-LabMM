add158e6df109415ca226629c47c289750e3f1cb
wdftr <ikhbal.madrid17@gmail.com>
2021-04-07 11:31:25 +0700

    add message to form barang

--- app/Http/Requests/FormBarangRequest.php
+++ app/Http/Requests/FormBarangRequest.php
@@ -33,4 +33,30 @@ class FormBarangRequest extends FormRequest
             'tanggal_pengembalian'  => ['date', 'after_or_equal:tanggal_peminjaman']
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'nama_peminjam.required'                => 'Mohon isi field Nama anda',
+            'nim.required'                          => 'Mohon isi field NIM/NIP anda',
+            'nim.regex'                             => 'Mohon isi field dengan angka 0-9',
+            'nim.min'                               => 'NIM/NIP minimal 8 angka',
+            'email.required'                        => 'Mohon isi field Email',
+            'email.email'                           => 'Mohon masukkan email dengan format yang tepat',
+            'no_hp.max'                             => 'No HP maksimal 13 angka',
+            'no_hp.min'                             => 'No HP minimal 10 angka',
+            'no_hp.required'                        => 'Mohon isi field No HP',
+            'no_hp.regex'                           => 'Mohon isi field dengan angka 0-9',
+            'afiliasi.required'                     => 'Mohon isi field Afiliasi',
+            'tanggal_peminjaman.required'           => 'Mohon isi field Tanggal Peminjaman',
+            'tanggal_peminjaman.date'               => 'Mohon isi field Tanggal Peminjaman dengan tanggal',
+            'tanggal_pengembalian.required'         => 'Mohon isi field Tanggal Pengembalian',
+            'tanggal_pengembalian.after_or_equal'   => 'Mohon isi field Tanggal Pengembalian sama atau setalah Tanggal Peminjaman'
+        ];
+    }
 }
