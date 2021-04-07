662965fcf0486caa5abec3f086d4dfa8a203f6a3
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-04-07 18:06:55 +0700

    fix verifikasi controller

--- app/Http/Controllers/Dashboard/VerifikasiController.php
+++ app/Http/Controllers/Dashboard/VerifikasiController.php
@@ -7,17 +7,15 @@ use App\Models\CalonAsprak;
 use App\Models\DaftarMataKuliah;
 use App\Models\PenilaianAsprak;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class VerifikasiController extends Controller
 {
     public function index()
     {
         $aspraks = CalonAsprak::latest()->get();
-        $matkuls = PenilaianAsprak::with('calonasprak')->get();
-        // dd(DB::table('penilaian_aspraks')->join('mata_kuliahs', 'mata_kuliahs.mata_kuliah_id', '=', 'penilaian_aspraks.mata_kuliah_id')->join('daftar_mata_kuliahs', 'daftar_mata_kuliahs.id', '=', 'mata_kuliahs.mata_kuliah_id')->select('daftar_mata_kuliahs.nama')->first());
-
-        return view('dashboard.pendaftaran.verifikasi.index', compact('aspraks', 'matkuls'));
+        $pilihans = PenilaianAsprak::with('matakuliah')->get();
+        $daftar_matkuls = DaftarMataKuliah::has('matakuliahs')->get();
+        return view('dashboard.pendaftaran.verifikasi.index', compact('aspraks', 'pilihans', 'daftar_matkuls'));
     }
 
     public function verifikasi(Request $request)
