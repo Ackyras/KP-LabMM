da42db22fed1bd3df149d5db8243d023b8e7f988
wdftr <ikhbal.madrid17@gmail.com>
2021-04-07 11:31:55 +0700

    add message to form ruangan

--- app/Http/Requests/FormRuanganRequest.php
+++ app/Http/Requests/FormRuanganRequest.php
@@ -28,13 +28,45 @@ class FormRuanganRequest extends FormRequest
             'nim'                   => ['required', 'regex:/[0-9]+/', 'min:8'],
             'email'                 => ['email', 'required'],
             'no_hp'                 => ['max:13', 'required', 'regex:/[0-9]+/', 'min:10'],
-            'afiliasi'              => ['required', 'string'],
-            'ruang_lab'             => ['required', 'string'],
-            'mata_kuliah'           => ['required', 'string'],
-            'kode_matkul'           => ['required', 'string'],
-            'dosen'                 => ['required', 'string'],
-            'waktu'                 => ['required'],
+            'afiliasi'              => ['required'],
+            'ruang_lab'             => ['required'],
+            'mata_kuliah'           => ['required'],
+            'kode_matkul'           => ['required'],
+            'dosen'                 => ['required'],
+            'waktu'                 => ['required', 'in:07:00:00,09:00:00,13:00:00,15:00:00'],
             'hari'                  => ['in:Senin,Selasa,Rabu,Kamis,Jumat,Sabtu,Minggu', 'required'],
+            'keterangan'            => ['required']
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'nama_peminjam.required'                => 'Mohon isi field Nama anda',
+            'nim.required'                          => 'Mohon isi field NIM/NIP anda',
+            'nim.regex'                             => 'Mohon isi field dengan angka 0-9',
+            'nim.min'                               => 'NIM/NIP minimal 8 angka',
+            'email.required'                        => 'Mohon isi field Email',
+            'email.email'                           => 'Mohon masukkan email dengan format yang tepat',
+            'no_hp.max'                             => 'No HP maksimal 13 angka',
+            'no_hp.min'                             => 'No HP minimal 10 angka',
+            'no_hp.required'                        => 'Mohon isi field No HP',
+            'no_hp.regex'                           => 'Mohon isi field dengan angka 0-9',
+            'afiliasi.required'                     => 'Mohon isi field Afiliasi',
+            'ruang_lab.required'                    => 'Mohon pilih ruangan lab',
+            'mata_kuliah.required'                  => 'Mohon isi field mata kuliah',
+            'kode_matkul.required'                  => 'Mohon isi field kode mata kuliah',
+            'dosen.required'                        => 'Mohon isi field dosen',
+            'waktu.required'                        => 'Mohon pilih waktu ruangan',
+            'hari.required'                         => 'Mohon pilih hari ruangan',
+            'waktu.in'                              => 'Pilih waktu antara 07:00, 09:00, 13:00, atau 15:00',
+            'hari.in'                               => 'Pilih hari dari Senin sampai Minggu',
+            'keterangan.required'                   => 'Mohon isi field keterangan'
         ];
     }
 }
