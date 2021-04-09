78f5d65646ff19494d85ed81d2a88987aed6a3e7
wdftr <ikhbal.madrid17@gmail.com>
2021-04-09 17:06:23 +0700

    update verifikasi controller

--- app/Http/Controllers/Dashboard/VerifikasiController.php
+++ app/Http/Controllers/Dashboard/VerifikasiController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\CalonAsprak;
 use App\Models\DaftarMataKuliah;
+use App\Models\MataKuliah;
 use App\Models\PembukaanAsprak;
 use App\Models\PenilaianAsprak;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -21,7 +23,7 @@ class VerifikasiController extends Controller
 
     public function index()
     {
-        $aspraks = CalonAsprak::where('periode', $this->pembukaan_id)
+        $aspraks = CalonAsprak::where('periode', $this->pembukaan_id->id)
             ->where('status', 0)
             ->orderBy('created_at', 'desc')
             ->simplePaginate(20);
@@ -32,22 +34,43 @@ class VerifikasiController extends Controller
 
     public function verifikasiberkas(Request $request)
     {
+        $calon = CalonAsprak::where('id', $request->input('id'))->first();
         switch ($request->input('action')) {
             case '0':
-                CalonAsprak::where('id', $request->input('id'))->delete();
+                CalonAsprak::where('id', $calon->id)->delete();
                 break;
             case '1':
-                // KIRIM NOTIFIKASI
-            case '2':
-                $password = uniqid();
-                CalonAsprak::where('id', $request->input('id'))
+                CalonAsprak::where('id', $calon->id)
                     ->update(
                         [
-                            'password'  => bcrypt($password),
-                            'status'    => 1
+                            'status'    => 2
                         ]
                     );
-                // KIRIM NOTIFIKASI
+                //KIRIM NOTIFIKASI TIDAK LULUS BERKAS
+                break;
+            case '2':
+                $password = uniqid();
+                DB::transaction(
+                    function () use ($password, $calon) {
+                        CalonAsprak::where('id', $calon->id)
+                            ->update(
+                                [
+                                    'password'  => bcrypt($password),
+                                    'status'    => 1
+                                ]
+                            );
+                        $akun = User::create(
+                            [
+                                'name'      => $calon->nama,
+                                'username'  => preg_replace("/\s+/", "", strtolower($calon->nama . '.' . $calon->nim)),
+                                'role'      => 'calonasprak',
+                                'email'     => $calon->email,
+                                'password'  => bcrypt($password)
+                            ]
+                        );
+                        // KIRIM NOTIFIKASI LULUS
+                    }
+                );
                 break;
         }
 
@@ -69,7 +92,7 @@ class VerifikasiController extends Controller
         PenilaianAsprak::where('id', $request->input('penilaian_id'))
             ->update(
                 [
-                    'nilai' => $request->nilai
+                    'nilai' => $request->input('nilai')
                 ]
             );
         return redirect()->route('asprak.nilai.index');
@@ -77,25 +100,46 @@ class VerifikasiController extends Controller
 
     public function verifikasilulus(Request $request)
     {
+        $calon = CalonAsprak::where('id', $request->input('id'))->first();
         switch ($request->input('action')) {
             case '1':
-                CalonAsprak::where('id', $request->input('id'))
-                    ->update(
-                        [
-                            'status'    => '3'
-                        ]
-                    );
+                DB::transaction(
+                    function () use ($calon, $request) {
+                        CalonAsprak::where('id', $calon->id)
+                            ->update(
+                                [
+                                    'status'    => '3'
+                                ]
+                            );
+                        User::where('email', $calon->email)->delete();
+                        $lulus = PenilaianAsprak::where('calon_asprak_id', $calon->id)
+                            ->where('mata_kuliah_id', $request->input('mata_kuliah_id'))
+                            ->update(['lulus' => 1]);
+                        $lulus =  PenilaianAsprak::where('calon_asprak_id', $calon->id)
+                            ->where('mata_kuliah_id', $request->input('mata_kuliah_id'))
+                            ->first();
+                        $mata_kuliah = MataKuliah::where('id', $lulus->mata_kuliah_id)->first();
+                        $mata_kuliah = DaftarMataKuliah::where('id', $mata_kuliah->mata_kuliah_id)
+                            ->pluck('nama')
+                            ->first();
+                        // KIRIM NOTIFIKASI LULUS ASPRAK
+                    }
+                );
                 break;
-                // KIRIM NOTIFIKASI LULUS
             case '2':
-                CalonAsprak::where('id', $request->input('id'))
-                    ->update(
-                        [
-                            'status'    => '4'
-                        ]
-                    );
+                DB::transaction(
+                    function () use ($calon) {
+                        CalonAsprak::where('id', $calon->id)
+                            ->update(
+                                [
+                                    'status'    => '3'
+                                ]
+                            );
+                        User::where('email', $calon->email)->delete();
+                        // KIRIM NOTIFIKASI LULUS TIDAK LULUS ASPRAK
+                    }
+                );
                 break;
-                // KIRIM NOTIFIKASI TIDAK LULUS
         }
     }
 }
