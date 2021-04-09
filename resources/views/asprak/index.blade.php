feb67dbabeeb629ebcd09c0c71722b0da8787a6e
wdftr <ikhbal.madrid17@gmail.com>
2021-04-09 17:07:44 +0700

    index view asprak

--- resources/views/asprak/index.blade.php
+++ resources/views/asprak/index.blade.php
@@ -1,4 +1,12 @@
 @extends('master.master')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/asprak/home.css') }}">
+@endsection
+
 @section('content')
+<div class="row">
+    <h4 class="p-2 title-header">Pengumuman</h4>
+    <div class="line"></div>
+</div>
 @endsection
