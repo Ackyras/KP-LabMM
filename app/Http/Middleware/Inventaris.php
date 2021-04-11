1ea05b2468740007a2986213da7278afcfd5b3a8
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-04-11 16:04:43 +0700

    inventaris middleware

---
+++ app/Http/Middleware/Inventaris.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Inventaris
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()) {
+            $role = Auth::user()->role;
+            if ($role == 'superadmin' or $role == 'inventaris') {
+                return $next($request);
+            } else {
+                return abort(403);
+            }
+        } else {
+            return redirect('login');
+        }
+    }
+}
