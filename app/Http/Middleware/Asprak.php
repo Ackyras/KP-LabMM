9d78f7f90189937438220f95b688ed076a2250b8
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-04-11 16:03:46 +0700

    update Middleware

--- app/Http/Middleware/Asprak.php
+++ app/Http/Middleware/Asprak.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class Admin
+class Asprak
 {
     /**
      * Handle an incoming request.
@@ -19,7 +19,7 @@ class Admin
     {
         if (Auth::check()) {
             $role = Auth::user()->role;
-            if ($role == 'superadmin' or $role == 'ruangan' or $role == 'inventaris' or $role == 'dosen' or $role == 'asprak') {
+            if ($role == 'superadmin' or $role == 'asprak') {
                 return $next($request);
             } else {
                 return abort(403);
--- app/Http/Middleware/SuperAdmin.php
+++ app/Http/Middleware/SuperAdmin.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class Admin
+class SuperAdmin
 {
     /**
      * Handle an incoming request.
@@ -18,8 +18,7 @@ class Admin
     public function handle(Request $request, Closure $next)
     {
         if (Auth::check()) {
-            $role = Auth::user()->role;
-            if ($role == 'superadmin' or $role == 'ruangan' or $role == 'inventaris' or $role == 'dosen' or $role == 'asprak') {
+            if (Auth::user()->role == 'superadmin') {
                 return $next($request);
             } else {
                 return abort(403);
