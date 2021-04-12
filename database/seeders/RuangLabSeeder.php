195f44f0bf024bda6fb93a612402caaa278dd23d
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-04-13 00:01:11 +0700

    add slug ruangan lab and update seeder

--- database/seeders/RuangLabSeeder.php
+++ database/seeders/RuangLabSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\RuangLab;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class RuangLabSeeder extends Seeder
 {
@@ -18,7 +19,8 @@ class RuangLabSeeder extends Seeder
             [
                 'ruang'     => 'Labtek 1 Lantai 2',
                 'lokasi'    => 'Labtek 1',
-                'status'    => 'Baik'
+                'status'    => 'Baik',
+                'slug'      => Str::slug('Labtek 1 Labtek 1 Lantai 2')
             ]
         );
     }
