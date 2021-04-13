73afb72b3181b176ef373b7b4c7d7137aa68763e
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-04-13 22:54:47 +0700

    update index list ruangan client

--- resources/views/ruangan/index.blade.php
+++ resources/views/ruangan/index.blade.php
@@ -12,30 +12,32 @@
 <div class="row row-2">
     <div class="card mx-auto">
         <div class="card-body align-items-center d-flex">
-            <table class="table table-bordered table-hover">
-                <thead class="thead">
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">Ruangan</th>
-                        <th scope="col">Lokasi</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse($ruangs as $ruang)
-                    <tr>
-                        <th scope="row">{{ $loop->iteration }}</th>
-                        <td>{{ $ruang->ruang }}</td>
-                        <td>{{ $ruang->lokasi }}</td>
-                        <td><a href="{{ route('ruangan.show', $ruang->slug) }}" class="btn btn-submit">Lihat Jadwal</a></td>
-                    </tr>
-                    @empty
-                    <tr>
-                        <th colspan="4">Tidak ada jadwal ruangan</th>
-                    </tr>
-                    @endforelse
-                </tbody>
-            </table>
+            <div class="table-responsive w-100">
+                <table class="table table-bordered table-hover">
+                    <thead class="thead">
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Ruangan</th>
+                            <th scope="col">Lokasi</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($ruangs as $ruang)
+                        <tr>
+                            <th scope="row">{{ $loop->iteration }}</th>
+                            <td>{{ $ruang->ruang }}</td>
+                            <td>{{ $ruang->lokasi }}</td>
+                            <td><a href="{{ route('ruangan.show', $ruang->slug) }}" class="btn btn-submit">Lihat Jadwal</a></td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <th colspan="4">Tidak ada jadwal ruangan</th>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
         </div>
         {{ $ruangs->links() }}
     </div>
