b272e3bd68a09464341821480291c26fdf6352a3
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-04-14 20:51:56 +0700

    mail verifikasi peminjaman ruangan

---
+++ resources/views/mail/ruangan.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
+    <title>Verifikasi Peminjaman Ruangan</title>
+</head>
+
+<body style="box-sizing: border-box; font-family: 'Quicksand', sans-serif;">
+    @if($content['validasi'] == 0)
+    <p>Peminjaman Ruangan Anda di terima</p>
+    <h4>Data</h4>
+    <ul>
+        <li>Nama : {{$content['nama']}}</li>
+        <li>Nim / NIP : {{$content['nim']}}</li>
+        <li>Afiliasi : {{$content['afiliasi']}}</li>
+        <li>Lokasi : {{$content['lokasi']}}</li>
+        <li>Ruang : {{$content['ruang']}}</li>
+        <li>Mata Kuliah : {{$content['matkul']}}</li>
+        <li>Hari : {{$content['hari']}}</li>
+        <li>Waktu : {{$content['waktu']}}</li>
+        <li>Keterangan : {{$content['keterangan']}}</li>
+    </ul>
+    @else
+    <p>Maaf, peminjaman Ruangan Anda di tolak karena {{$content['pesan']}}. Silahkan isi ulang form dengan waktu yang kosong. Terima kasih.</p>
+    <h4>Data</h4>
+    <ul>
+        <li>Nama : {{$content['nama']}}</li>
+        <li>Nim / NIP : {{$content['nim']}}</li>
+        <li>Afiliasi : {{$content['afiliasi']}}</li>
+        <li>Lokasi : {{$content['lokasi']}}</li>
+        <li>Ruang : {{$content['ruang']}}</li>
+        <li>Mata Kuliah : {{$content['matkul']}}</li>
+        <li>Hari : {{$content['hari']}}</li>
+        <li>Waktu : {{$content['waktu']}}</li>
+        <li>Keterangan : {{$content['keterangan']}}</li>
+    </ul>
+    @endif
+</body>
+
+</html>
