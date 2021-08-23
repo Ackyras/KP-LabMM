b5cebac3fedd6b202bc4681f03e8d7e32e1975e0
ackyras <ackyrasibarani@gmail.com>
2021-08-23 08:00:57 +0700

    update API

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AsprakPresensiStoreRequest;
+use App\Models\Asprak;
+use App\Models\QrCode;
 use App\Models\User;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class UserController extends Controller
 {
@@ -22,8 +28,105 @@ class UserController extends Controller
         return response()->json(['msg' => 'Login Berhasil', 'status' => true, 'error' => null, 'user' => $user]);
     }
 
-    public function userList(){
+    public function presensi(AsprakPresensiStoreRequest $req)
+    {
+        $today = today();
+        $now = now();
+        $qr = QrCode::where('token', '=', $req->token)->first();
+        $asprak = Asprak::find($req->id);
+        // return response()->json($qr->valid_until);
+        // dd($qr);
+        if (!$qr) {
+            return response()->json(['msg'  =>  'Presensi gagal', 'status' => false, 'error' => 'QRCode tidak valid'], 404);
+        } else {
+            if ($qr->valid_until < $now) {
+                return response()->json(['msg'  =>  'Presensi gagal', 'status' => false, 'error' => 'QRCode sudah expired'], 406);
+                // } else if(){
+
+            } else {
+                DB::beginTransaction();
+                try {
+                    //code...
+                    $asprak->presensi()->syncWithoutDetaching(
+                        $qr,
+                        [
+                            'created_at'    =>  $now,
+                            'updated_at'    =>  $now,
+                        ]
+                    );
+                    DB::commit();
+                } catch (\Throwable $th) {
+                    //throw $th;
+                    DB::rollback();
+                    return response()->json(['msg'  =>  'Presensi gagal', 'status'  =>  false, 'error'  =>  'Terjadi kesalahan pada server, harap menunggu beberapa saat lagi'], 500);
+                }
+                return response()->json(['msg'  =>  'Presensi berhasil', 'status' => true, 'error' => 'null'], 200);
+            }
+        }
+    }
+
+    public function update(Request $request)
+    {
+        $error = 'Email telah digunakan';
+
+        $email = User::where('id', $request->id)->where('email', $request->email)->first();
+
+        if ($email) {
+            User::where('id', $request->id)->update(
+                [
+                    'nama'      => $request->nama,
+                    'email'     => $request->email,
+                    'kelas'     => $request->kelas
+                ]
+            );
+            $error = null;
+            if ($request->password != '') {
+                User::where('id', $request->id)->update(
+                    [
+                        'password'      => bcrypt($request->password),
+                    ]
+                );
+            }
+        }
+
+        $response = array('error' => $error);
+
+        return response()->json($response);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->flush();
+    }
+
+    public function isAdmin(Request $request)
+    {
+        $user = User::where('id', $request->id)->first();
+
+        $role = 0;
+
+        if ($user->role == '1') {
+            $role = 1;
+        }
+
+        $response = array('role' => $role);
+
+        return response()->json($response);
+    }
+
+    public function destroy(Request $request)
+    {
+        User::where('id', $request->id)->delete();
+
+        return response()->json("Sukses", 200);
+    }
+
+    public function userList()
+    {
         $user = User::all();
         return response($user);
     }
+
+    // public function
 }
---
+++ app/Http/Controllers/Dashboard/PresensiController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\QRStoreRequest;
+use App\Models\Asprak;
+use App\Models\QrCode;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PresensiController extends Controller
+{
+    public function daftar()
+    {
+        $qr = QrCode::paginate(10);
+        return view('dashboard.pendaftaran.presensi.list', compact('qr'));
+    }
+
+    public function log()
+    {
+        $presensi = Asprak::with('presensi')->withCount('presensi')->paginate(15);
+        return view('dashboard.pendaftaran.presensi.log', compact('presensi'));
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $asprak = Asprak::paginate(10);
+        return view('dashboard.pendaftaran.presensi.index', compact('asprak'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('dashboard.pendaftaran.presensi.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(QRStoreRequest $request)
+    {
+        //
+        DB::beginTransaction();
+        try {
+            //code...
+            $qr = QrCode::create($request->validated());
+            DB::commit();
+        } catch (\Throwable $th) {
+            throw $th;
+            DB::rollback();
+            return back()->with('error', 'Gagal menambahakan Data');
+        }
+        return redirect()->route('presensi.list')->with('success', 'Berhasil menambahkan QRCode');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $qr = QrCode::find($id);
+        DB::beginTransaction();
+        try {
+            //code...
+            $qr->destroy();
+            DB::commit();
+        } catch (\Throwable $th) {
+            //throw $th;
+            DB::rollback();
+            return back()->with('errror', 'Gagal menghapus Data!');
+        }
+        return back()->with('success', 'Data berhasil dihapus');
+    }
+}
