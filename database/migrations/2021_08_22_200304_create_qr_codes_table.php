ba316cda6a33f5ed4cb1c9e4e5de4f80ef21cce3
ackyras <ackyrasibarani@gmail.com>
2021-08-23 08:02:17 +0700

    add new presensi table

---
+++ database/migrations/2021_08_22_200304_create_qr_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQrCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('qr_codes', function (Blueprint $table) {
+            $table->id();
+            $table->date('valid_for');
+            $table->dateTime('valid_until');
+            $table->string('token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('qr_codes');
+    }
+}
