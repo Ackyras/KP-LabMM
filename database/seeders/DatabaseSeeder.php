ac1b9577d857e3f05184d2aa21d11ae394ebc103
ackyras <ackyrasibarani@gmail.com>
2021-08-25 04:09:27 +0700

    add qrcode seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(akunSeeder::class);
         $this->call(asprakSeeder::class);
+        $this->call(QRCodeSeeder::class);
         $this->call(BarangSeeder::class);
         $this->call(FormPeminjam::class);
         $this->call(PeminjamanBarang::class);
