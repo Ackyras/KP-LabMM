3d2817f9ea945f6418a7cb7233ebef8ad12ea53d
ackyras <ackyrasibarani@gmail.com>
2021-09-04 03:45:12 +0700

    add test qr

--- database/seeders/QRCodeSeeder.php
+++ database/seeders/QRCodeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\QrCode;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class QRCodeSeeder extends Seeder
 {
@@ -15,10 +16,17 @@ class QRCodeSeeder extends Seeder
     public function run()
     {
         //
-        $qr = QrCode::create([
-            'valid_for'     =>  today(),
-            'valid_until'   =>  '2021-08-25 04:27:00',
-            'token'         =>  '7IGkpkqWwbGzitnb4wmE',
-        ]);
+        $qr = QrCode::create(
+            [
+                'valid_for'     =>  today(),
+                'valid_until'   =>  '2021-08-25 04:27:00',
+                'token'         =>  '7IGkpkqWwbGzitnb4wmE',
+            ],
+            [
+                'valid_for'     =>  today(),
+                'valid_until'   =>  '2021-12-25 04:27:00',
+                'token'         =>  'testtesttesttesttest',
+            ]
+        );
     }
 }
